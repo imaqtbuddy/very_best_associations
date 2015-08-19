93ebf5e01412dc714d69063c89eb8454045ffae0
Andi Fuess <afuess@chicagobooth.edu>
2015-08-18 22:26:12 -0500

    final commit for homework eight

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,4 +1,5 @@
 class Neighborhood < ActiveRecord::Base
   validates :name, :presence=>true, :uniqueness=>{:scope=>:city}, :presence=>true
   has_many :venues, :dependent => :destroy
+  has_many :favorites, :through => :venues
 end
